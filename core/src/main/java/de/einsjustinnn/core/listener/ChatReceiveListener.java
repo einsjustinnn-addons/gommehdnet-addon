d93dfa96a8f78e55806f902fd981f0ce25e7061d
EinsJustinnn <z0ck3r98hd@gmail.com>
2024-04-02 13:31:18 +0200

    add support for en_us

--- core/src/main/java/de/einsjustinnn/core/listener/ChatReceiveListener.java
+++ core/src/main/java/de/einsjustinnn/core/listener/ChatReceiveListener.java
@@ -6,15 +6,19 @@ import net.labymod.api.event.client.chat.ChatReceiveEvent;
 
 public class ChatReceiveListener {
 
+  private static final String[] gameStarted = new String[] {"The game has started!", "Die Runde beginnt!"};
+
   @Subscribe
   public void onChatReceive(ChatReceiveEvent event) {
 
-    String plainText = event.chatMessage().getPlainText();
+    String plainText = event.chatMessage().getPlainText().toLowerCase();
 
-    if (plainText.equalsIgnoreCase("[BedWars] Die Runde beginnt!") || plainText.equalsIgnoreCase("[ClassicBW] Die Runde beginnt!")) {
-      GommeAddon.bedwars.setRunning(true);
-      GommeAddon.bedwars.setStarted(System.currentTimeMillis());
+    for (String message : gameStarted) {
+      if (plainText.equalsIgnoreCase("[Bedwars] " + message) || plainText.equalsIgnoreCase("[ClassicBW] " + message)) {
+        GommeAddon.bedwars.setRunning(true);
+        GommeAddon.bedwars.setStarted(System.currentTimeMillis());
+        break;
+      }
     }
-
   }
 }
